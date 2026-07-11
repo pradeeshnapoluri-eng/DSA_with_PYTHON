9304fe49c5aa7374070303c238c2b8b80283d704
pacha-hygge <pacha@hygge.energy>
2026-07-11 19:15:38 +0530

    updated

--- ARRAY/08_max_consecutive_ones.py
+++ ARRAY/08_max_consecutive_ones.py
@@ -1,12 +1,27 @@
-arr=[1,1,1,0,1,1,1,1,0,1,1,1,1,1,1,0,1]
-count=0
-max_count=0
-for i in arr:
-    if i==1:
-        count+=1
-        if count > max_count:
-            max_count = count
-    else:
+# arr=[1,0,1,1,0,1]
+# count=0
+# max_count=0
+# for i in arr:
+#     if i==1:
+#         count+=1
+#         if count > max_count:  ##elago great ae
+#             max_count = count
+#     else:
+#         count=0
+# print(count)
+# print(max_count)    
+
+class Solution:
+    def findMaxConsecutiveOnes(self, nums: list[int]) -> int:
         count=0
-print(count)
-print(max_count)    
+        max_nums=0
+        for i in nums:
+            if i == 1:
+                count += 1
+                if count > max_nums:
+                    max_nums=count
+            else:
+                count=0
+        return count
+        return max_nums
+            
--- ARRAY/09_find_number_appears_once.py
+++ ARRAY/09_find_number_appears_once.py
@@ -1,8 +1,19 @@
-arr=[1,2,2,3,3,3,4,4]
-found=False
-for i in range(1,8):
-    if arr.count(i) == 1:
-        found=True
-        print('once appeared:',i)
-if found == False:
-    print("No single element")
+# arr=[1,2,2,3,3,3,4,4]
+# found=False
+# for i in range(1,8):
+#     if arr.count(i) == 1:
+#         found=True
+#         print('once appeared:',i)
+# if found == False:
+#     print("No single element")
+
+class Leetcode:
+    def single_element(self,nums=list[int]):
+        result=0
+        for i in nums:
+            result ^= i
+        return result
+sol=Leetcode()
+# nums=[4,1,2,1,2]
+nums=[3,3,4,1,2,4,5,5,2]
+print(sol.single_element(nums))
